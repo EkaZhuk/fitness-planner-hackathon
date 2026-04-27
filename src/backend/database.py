11cca668c8c62ed123af473f50383637cfe1c562
Mikhail Petrov <petrov.m.a02@mail.ru>
2026-04-27 22:24:32 +0300

    add predict and progress endpoints

--- src/backend/database.py
+++ src/backend/database.py
@@ -2,6 +2,10 @@ import sqlite3
 from pydantic import BaseModel
 from datetime import date
 
+class Goal(BaseModel):
+    goal_km: float
+    goal_deadline: date
+
 class Training(BaseModel):
     training_date: date
     distance_km: float
@@ -45,16 +49,44 @@ class TrainingDataBase:
     
     def get_all(self):
         self.cursor.execute('''
-            SELECT * FROM trainings;
+            SELECT * 
+            FROM trainings;
         ''')
         return [Training(training_date=training[1],
                          distance_km=training[2],
                          duration_min=training[3],
                          notes=training[4]) for training in self.cursor.fetchall()]
 
+    def get_results_by_week(self, days: int):
+        self.cursor.execute(f'''
+            SELECT *, date(end_of_the_week, '-6 days') as start_of_the_week
+            FROM (SELECT date(training_date, 'weekday 0') as end_of_the_week,
+                         ROUND(SUM(distance_km), 2) as total_distance_km, 
+                         ROUND(SUM(duration_min), 2) as total_duration_min
+                  FROM trainings
+                  WHERE training_date > (SELECT date('now', '-{days} day'))
+                  GROUP BY end_of_the_week)
+                  ORDER BY end_of_the_week;
+        ''')
+        return [{'week_start': week[3],
+                 'week_end': week[0],
+                 'total_distance_km': week[1],
+                 'total_duration_min': week[2]} for week in self.cursor.fetchall()]
+
+
 if __name__ == '__main__':
     db = TrainingDataBase()
     db.add_training(Training(training_date=date.today(),
                             distance_km=2.2,
                             duration_min=2.2,))
+    db.add_training(Training(training_date=date.today(),
+                            distance_km=2.5,
+                            duration_min=2.1,))
+    db.add_training(Training(training_date=date(2026, 4, 5),
+                            distance_km=2.2,
+                            duration_min=2.2,))
+    db.add_training(Training(training_date=date(2026, 3, 25),
+                            distance_km=2.2,
+                            duration_min=2.2,))
     print(db.get_all())
+    print(db.get_results_by_week(30))
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
-from database import Training, TrainingDataBase
+from database import Goal, Training, TrainingDataBase
 from datetime import date
+import sys
 import uvicorn
+
+sys.path.append('src/ml')
+from predict import predict_finish_date
 
 app = FastAPI(title='Fitness Planner')
 
 db = TrainingDataBase()
+goal = Goal(goal_km=10, goal_deadline=date(2026, 6, 1))
 
 @app.get('/')
 def home_page():
-    return 'Hello, World!'
+    return 'Fitness Planner'
 
 @app.get('/trainings')
 def get_trainings_list():
@@ -23,11 +28,16 @@ def add_training(training: Training):
 # progress?days=30
 @app.get('/progress')
 def get_progress(days: int=30):
-    return f'{days} days'
+    # Прогресс за последние days дней разбитый по неделям
+    return db.get_results_by_week(days)
 
 @app.get('/goal')
 def get_progress():
-    return {'goal_km': 10, 'goal_deadline': date(2026, 6, 1)}
+    return goal
+
+@app.get('/predict')
+def get_prediction():
+    return predict_finish_date(goal.goal_km, str(goal.goal_deadline))
 
 if __name__ == '__main__':
     uvicorn.run(app)
